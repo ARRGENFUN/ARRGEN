45c26ef69a37110b6f8e4906a72e40a372e298e5
MELOEN <eduardermakov769@gmail.com>
2025-11-12 22:20:42 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os        BUILME
+import os        MELOEN
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
